66384ccb0cb87ae0dd77e876412c7e49de42a49c
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-12-09 19:50:12 -0300

    imersao-fullcycle-microservicos

--- walletcore-api/internal/database/transaction_go_test.go
+++ walletcore-api/internal/database/transaction_go_test.go
@@ -13,8 +13,6 @@ type TransactionDBTestSuite struct {
 	suite.Suite
 
 	DB            *sql.DB
-	AccountDB     *database.AccountDB
-	CustomerDB    *database.CustomerDB
 	TransactionDB *database.TransactionDB
 }
 
@@ -23,19 +21,13 @@ func (testSuite *TransactionDBTestSuite) SetupSuite() {
 
 	testSuite.Nil(err)
 
-	db.Exec("CREATE TABLE IF NOT EXISTS tb_customer (id varchar(255), name varchar(255), email varchar(255), created_at datetime, updated_at datetime)")
-	db.Exec("CREATE TABLE IF NOT EXISTS tb_account (id varchar(255), customer_id varchar(255), balance float, created_at datetime, updated_at datetime)")
 	db.Exec("CREATE TABLE IF NOT EXISTS tb_transaction (id varchar(255), account_id_from varchar(255), account_id_to varchar(255), amount float, created_at datetime)")
 
 	testSuite.DB = db
-	testSuite.AccountDB = database.NewAccountDB(db)
-	testSuite.CustomerDB = database.NewCustomerDB(db)
 	testSuite.TransactionDB = database.NewTransactionDB(db)
 }
 
 func (testSuite *TransactionDBTestSuite) TearDownSuite() {
-	testSuite.DB.Exec("DROP TABLE tb_customer")
-	testSuite.DB.Exec("DROP TABLE tb_account")
 	testSuite.DB.Exec("DROP TABLE tb_transaction")
 	testSuite.DB.Close()
 }
